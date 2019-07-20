c786b64d82385b138817bf792e2cb4424eea0557
Valena Del Valle <valenadelvalle4@gmail.com>
2019-07-20 00:22:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :name
+
+  def initialize(string)
+    @name = string
+  end
+  
+  def match(array)
+    array.select do |word| 
+      word.split("").sort == @name.split("").sort
+    end
+  end
+  
+end
